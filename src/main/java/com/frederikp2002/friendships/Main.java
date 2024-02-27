3e6ce4d99f4991e8f82459644c8803d33f21a012
Frederik P <frederikp2002@gmail.com>
2024-02-27 13:19:56 +0100

    Wrote the code for the MessageHandler. The MessageHandler has three methods: [+] loadMessages Used to detect if the messages.yml exist: if it does not exist, then it will be generated.
    [+] getMessage
    Used to get a string from the messages.yml file. If the key is not found, then you will be informed.
    
    [+] getMessageListFormatted
    Used to get a list from the messages.yml file with formatting.

--- src/main/java/com/frederikp2002/friendships/Main.java
+++ src/main/java/com/frederikp2002/friendships/Main.java
@@ -1,17 +1,22 @@
 package com.frederikp2002.friendships;
 
+import com.frederikp2002.friendships.handlers.IMessageHandler;
+import com.frederikp2002.friendships.handlers.implementations.MessageHandler;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
 
+    private IMessageHandler messageHandler;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        this.messageHandler = new MessageHandler(this);
+        getLogger().info(messageHandler.getMessage("plugin.enabled").content());
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        getLogger().info(messageHandler.getMessage("plugin.disabled").content());
     }
 }
